a8b229d7292a0c291c80d2daa8849abef6974276
Antonio <antonio.toche@utec.edu.pe>
2019-04-01 17:17:19 -0500

    v1.0

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+void NumberLines(int);
+void countryRepeat(string);
+int main()
+{
+    int nLines;
+    cin >> nLines;
+    NumberLines(nLines);
     return 0;
+}
+
+void NumberLines(int nLines)
+{
+    string country;
+    string girlLoved;
+    for (int i = 0; i <= nLines; i++)
+    {
+        cin >> country >> girlLoved;
+        countryRepeat(country);
+    }
 }
